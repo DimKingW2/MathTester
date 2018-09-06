b39a073ba74e3faf8c6582eeb1efceca3a21ed5f
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-09-06 07:43:43 -0400

    added code

---
+++ src/MathTester.java
@@ -0,0 +1,37 @@
+public class MathTester {
+    public static void main (String args[]){
+        String firstWord, secondWord;
+        int firstNumber, secondNumber, total;
+
+
+
+
+
+
+        firstWord = "Dog";
+        secondWord = "House";
+        firstNumber = 5;
+        secondNumber = 10;
+        total = firstNumber+secondNumber;
+        System.out.println(total);
+        total = total +5;
+        System.out.println(total);
+
+
+
+
+
+    /*    System.out.println(firstWord + secondWord);
+        System.out.println(firstWord + (firstNumber + secondNumber));
+        System.out.println("First number + second number is " + (firstNumber + secondNumber));
+        System.out.println("First number - second number is " + (firstNumber - secondNumber));
+        System.out.println("Second number / first number is " + (secondNumber / firstNumber));
+        System.out.println("First number * second number is " + (firstNumber * secondNumber));
+        System.out.println("Second number % first number is " + (secondNumber % firstNumber));
+*/
+
+
+
+
+    }
+}
